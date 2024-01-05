b34361df9a94a93cf5e0656f443e88b245640c25
Harsh Porwal <harsh30porwal@gmail.com>
2024-01-05 20:37:12 +0530

    Update models.py

--- reserveat/reserveatapp/models.py
+++ reserveat/reserveatapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class regadmin(models.Model):
+    firstname= models.CharField(max_length= 200)
+    lastname= models.CharField(max_length= 200)
+    email= models.EmailField(max_length= 200)
+    phoneno= models.BigIntegerField()
+    password= models.CharField(max_length= 250)
+
+    class meta:
+        db_table= 'admin-login'
+
